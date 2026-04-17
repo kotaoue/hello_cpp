8ba4184ff5d770d546380ffd82801deb6b2c8d1b
kotaoue <kotaoue@gmail.com>
2026-04-17 17:14:17 +0900

    Use flat black box with white label
    Style the main Fl_Box in hello_fltk/main.cpp by setting it to a flat box and applying a black background with a white label for improved contrast and a cleaner look. Adds box->box(FL_FLAT_BOX), box->color(FL_BLACK) and box->labelcolor(FL_WHITE).

--- hello_fltk/main.cpp
+++ hello_fltk/main.cpp
@@ -7,6 +7,9 @@ int main(int argc, char *argv[])
     Fl_Window *window = new Fl_Window(640, 480, "Hello World");
     Fl_Box *box = new Fl_Box(0, 0, 640, 480, "Hello, World!");
     box->labelsize(36);
+    box->box(FL_FLAT_BOX);
+    box->color(FL_BLACK);
+    box->labelcolor(FL_WHITE);
     window->end();
     window->show(argc, argv);
     int ret = Fl::run();
