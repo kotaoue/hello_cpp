3995e1a4386eaf34dcdb78bf58c5a0c11dc68169
kotaoue <kotaoue@gmail.com>
2026-04-17 17:15:06 +0900

    Reduce Fl_Box size to 320x240
    Update hello_fltk/main.cpp to construct the Fl_Box with dimensions 320x240 instead of 640x480. The label and styling are unchanged; this makes the box smaller than the window rather than filling it completely.

--- hello_fltk/main.cpp
+++ hello_fltk/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     Fl_Window *window = new Fl_Window(640, 480, "Hello World");
-    Fl_Box *box = new Fl_Box(0, 0, 640, 480, "Hello, World!");
+    Fl_Box *box = new Fl_Box(0, 0, 320, 240, "Hello, World!");
     box->labelsize(36);
     box->box(FL_FLAT_BOX);
     box->color(FL_BLACK);
