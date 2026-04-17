285b0583bead85fb40f82bf261921b796a5a0c57
kotaoue <kotaoue@gmail.com>
2026-04-17 09:21:57 +0900

    Update main.cpp

--- qt_hello/main.cpp
+++ qt_hello/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     QLabel label("Hello, World!");
-    label.setWindowTitle("Qt Hello World");
+    label.setWindowTitle("Hello World");
     label.resize(640, 480);
     label.setAlignment(Qt::AlignCenter);
     label.show();
